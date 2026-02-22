c2bb6df322a678d4e6b61209f5c4d3de5e727da2
Denokaz <kazmacideniz@gmail.com>
2026-02-22 18:13:22 +0300

    fix(layout): remove automatic location permission prompt on site load

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -3,7 +3,6 @@
 import { usePathname } from 'next/navigation';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import { LocationPermissionPrompt } from './LocationPermissionPrompt';
 
 export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
     const pathname = usePathname();
@@ -26,8 +25,6 @@ export default function LayoutWrapper({ children }: { children: React.ReactNode 
             <footer role="contentinfo">
                 <Footer />
             </footer>
-            {/* GPS Location Tracking - Optional, dismissable */}
-            <LocationPermissionPrompt />
         </>
     );
 }
